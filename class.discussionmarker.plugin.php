a84698bcb691416d9ba060abd012b83bb1baf354
bleistivt <bleistivt@users.noreply.github.com>
2015-06-25 16:07:39 +0200

    casening and fix for word boundaries

--- class.discussionmarker.plugin.php
+++ class.discussionmarker.plugin.php
@@ -1,4 +1,4 @@
-<?php if (!defined('APPLICATION'))  exit();
+<?php
 
 $PluginInfo['DiscussionMarker'] = array(
     'Name' => 'Discussion Marker',
@@ -6,89 +6,80 @@ $PluginInfo['DiscussionMarker'] = array(
     'Version' => '1.7',
     'SettingsUrl' => '/dashboard/settings/DiscussionMarker',
     'MobileFriendly' => true,
-    'Author' => "Peregrine",
+    'Author' => 'Peregrine',
     'License' => 'GNU GPL2'
 );
 
 class DiscussionMarkerPlugin extends Gdn_Plugin {
 
-    public function SettingsController_DiscussionMarker_Create($Sender) {
-        $Session = Gdn::Session();
-        $Sender->Title('Discussion Marker');
-        $Sender->AddSideMenu('plugin/discussionmarker');
-        $Sender->Permission('Garden.Settings.Manage');
-        $Sender->Form = new Gdn_Form();
-        $Validation = new Gdn_Validation();
-        $ConfigurationModel = new Gdn_ConfigurationModel($Validation);
-        $ConfigurationModel->SetField(array(
+    public function settingsController_discussionMarker_create($sender) {
+        $sender->title('Discussion Marker');
+        $sender->addSideMenu('plugin/discussionmarker');
+        $sender->permission('Garden.Settings.Manage');
+
+        $sender->Form = new Gdn_Form();
+        $validation = new Gdn_Validation();
+        $configurationModel = new Gdn_ConfigurationModel($validation);
+        $configurationModel->setField(array(
             'Plugins.DiscussionMarker.WordList',
             'Plugins.DiscussionMarker.AllowJump',
             'Plugins.DiscussionMarker.GroupLabels',
         ));
-        $Sender->Form->SetModel($ConfigurationModel);
+        $sender->Form->setModel($configurationModel);
 
-
-        if ($Sender->Form->AuthenticatedPostBack() === FALSE) {
-            $Sender->Form->SetData($ConfigurationModel->Data);
+        if ($sender->Form->authenticatedPostBack() && $sender->Form->save()) {
+            $sender->informMessage(T('Your settings have been saved.'));
         } else {
-            $Data = $Sender->Form->FormValues();
-
-            if ($Sender->Form->Save() !== FALSE)
-                $Sender->StatusMessage = T("Your settings have been saved.");
+            $sender->Form->setData($configurationModel->Data);
         }
-        $Sender->Render($this->GetView('discussionmarker-settings.php'));
+
+        $sender->render($this->getView('discussionmarker-settings.php'));
     }
 
-    public function DiscussionsController_BeforeDiscussionMeta_Handler($Sender) {
-        $this->DisplayMarker($Sender);
+    public function discussionsController_beforeDiscussionMeta_handler($sender, $args) {
+        $this->displayMarker($sender, $args);
     }
 
-    public function CategoriesController_BeforeDiscussionMeta_Handler($Sender) {
-        $this->DisplayMarker($Sender);
+    public function categoriesController_beforeDiscussionMeta_handler($sender, $args) {
+        $this->displayMarker($sender, $args);
     }
 
 
-    public function DiscussionsController_Render_Before($Sender) {
-        $Sender->AddCssFile('dm.css', 'plugins/DiscussionMarker');
-        if (C('Plugins.DiscussionMarker.AllowJump', FALSE) == "Scroll") {
-            $Sender->AddJsFile($this->GetResource('js/discussionmarker.js', FALSE, FALSE));
+    public function discussionsController_render_before($sender) {
+        $sender->addCssFile('dm.css', 'plugins/DiscussionMarker');
+        if (C('Plugins.DiscussionMarker.AllowJump', false) == 'Scroll') {
+            $sender->addJsFile($this->getResource('js/discussionmarker.js', false, false));
         }
     }
 
-    public function CategoriesController_Render_Before($Sender) {
-        $Sender->AddCssFile('dm.css', 'plugins/DiscussionMarker');
-        if (C('Plugins.DiscussionMarker.AllowJump', FALSE) == "Scroll") {
-            $Sender->AddJsFile($this->GetResource('js/discussionmarker.js', FALSE, FALSE));
+    public function categoriesController_render_before($sender) {
+        $sender->addCssFile('dm.css', 'plugins/DiscussionMarker');
+        if (C('Plugins.DiscussionMarker.AllowJump', false) == 'Scroll') {
+            $sender->addJsFile($this->getResource('js/discussionmarker.js', false, false));
         }
     }
 
+    protected function displayMarker($sender, $args) {
+        $title = val('Name', $args['Discussion']);
+        $markerArray = preg_split('/ *, */', C('Plugins.DiscussionMarker.WordList'));
 
-    protected function DisplayMarker($Sender) {
-        $Discussion = $Sender->EventArguments['Discussion'];
-        $title = val('Name', $Discussion);
+        foreach ($markerArray as $marker) {
+            if (!preg_match('/\b'.$marker.'\b/', $title)) {
+                continue;
+            }
 
-        // can also be used with discussion prefixes plugin.
-        // $Prefixes = explode (";", C('Plugins.PrefixDiscussion.Prefixes'));
-        $MarkerArray = explode(",", C('Plugins.DiscussionMarker.WordList'));
-
-        foreach ($MarkerArray as $Marker) {
-            $Marker = trim($Marker);
-
-            $pos = stripos($title, $Marker);
-            if (is_numeric($pos)) {
-                $tagmarker = preg_replace("/[\s]/","-",$Marker);
-                if (C('Plugins.DiscussionMarker.GroupLabels', FALSE)) {
-                    $LMarker = "DLP " .$Marker;
-                    $Marker =trim(ucwords(strtolower(T($LMarker))));
-                }
-                $Marker =trim(ucwords(strtolower($Marker)));
-                if (C('Plugins.DiscussionMarker.AllowJump', FALSE) != "Search") {
-                    echo Wrap($Marker, 'span', array('class' => "Tag DMarker DMarker-" . $tagmarker ));
-                } else {
-                    $searchurl = 'search&Search=' . preg_replace("/[\s]/","+",$Marker);
-                    echo Anchor(Wrap(Gdn_Format::Text($Marker), 'span',
-                        array('class' => "Tag DMarker DMarker-" . $tagmarker )),$searchurl);
-                }
+            $tagmarker = preg_replace('/[\s]/','-',$marker);
+            if (C('Plugins.DiscussionMarker.GroupLabels', false)) {
+                $lMarker = 'DLP ' .$marker;
+                $marker = ucwords(strtolower(T($lMarker)));
+            }
+            $marker = ucwords(strtolower($marker));
+            if (C('Plugins.DiscussionMarker.AllowJump', false) != 'Search') {
+                echo wrap($marker, 'span', array('class' => 'Tag DMarker DMarker-'.$tagmarker));
+            } else {
+                $searchurl = 'search&Search=' . preg_replace('/[\s]/', '+', $marker);
+                echo anchor(wrap(Gdn_Format::text($marker), 'span',
+                    array('class' => 'Tag DMarker DMarker-' . $tagmarker )), $searchurl);
             }
         }
     }
